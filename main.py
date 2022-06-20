ccb22761a798f1446d2faed24574ae7a1a260ee3
MKtm60 <71127103+MKtm60@users.noreply.github.com>
2022-06-20 15:25:34 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+from saagieapi import *
+saagie = SaagieApi(url_saagie="<url>",
+                   id_platform="1",
+                   user="<saagie-user-name>",
+                   password="<saagie-user-password>",
+                   realm="saagie")
